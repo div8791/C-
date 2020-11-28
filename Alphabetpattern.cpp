13b85547576ad5595543e6c048024c670e3b30e1
Divyarth Singh <73840080+div8791@users.noreply.github.com>
2020-11-28 13:21:39 +0530

    Add files via upload

---
+++ Alphabetpattern.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cin>>n;
+	for(int row=1; row<=n; row++){
+		char ch='A';
+		for(int col=1; col<=row; col++){
+			cout<<ch<<' ';
+			ch++;
+		}
+		cout<<endl;
+	}
+	return 0;
+}
---
+++ printvalueofASCII.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{   
+
+	for(int c=65; c<=250; c=c+1){
+		char ch=c;
+		cout<<c<<":"<<ch<<endl;
+	}
+	return 0;
+}
