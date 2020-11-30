48a899b4eb55c635f1a593a448e9a6cc4ecc2d19
Divyarth Singh <73840080+div8791@users.noreply.github.com>
2020-11-30 20:10:34 +0530

    Add files via upload

---
+++ simplecalculator.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a, b;
+	cin>>a>>b;
+	char op;
+	cin>>op;
+	switch(op)
+	{
+		case'+':
+		cout<<a+b<<endl;
+		break;
+
+		case'-':
+		cout<<a-b<<endl;
+		break;
+
+		case'*':
+		cout<<a*b<<endl;
+		break;
+
+		case'/':
+		cout<<a/b<<endl;
+		break;
+
+		default:
+		cout<<"by default"<<endl;
+	}
+	return 0;
+}
